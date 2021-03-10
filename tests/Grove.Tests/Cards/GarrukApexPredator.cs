a9118fb98287a81469c92dea2c714bf3113c36ea
Callumhutchy <callumhutchie@hotmail.co.uk>
2021-03-10 17:05:18 +0000

    Added unit test for Sigil of the Empty Throne and added some extra cases for Garruk Apex Predator

--- tests/Grove.Tests/Cards/GarrukApexPredator.cs
+++ tests/Grove.Tests/Cards/GarrukApexPredator.cs
@@ -2,26 +2,56 @@
 {
   using Infrastructure;
   using Xunit;
+  using System.Linq;
 
   public class GarrukApexPredator
   {
     public class Ai : AiScenario
     {
+
       [Fact]
-      public void CreatureAttackingGet55()
+      public void Plus1()
+      {
+        var garruk = C("Garruk, Apex Predator");
+        Battlefield(P1, garruk.AddCounters(1, CounterType.Loyalty));
+        Battlefield(P2, "Grizzly Bears");
+
+        RunGame(3);
+
+        Equals(3, C(garruk).Loyalty);
+        Equals(2, P1.Battlefield.Creatures.Count());
+      }
+
+      [Fact]
+      public void Minus3()
+      {
+        var garruk = C("Garruk, Apex Predator");
+        Battlefield(P1, garruk.AddCounters(4, CounterType.Loyalty));
+        Battlefield(P2, "Grizzly Bears");
+        P1.Life = 1;
+        RunGame(2);
+
+        Equals(1, C(garruk).Loyalty);
+        Equals(3, P2.Life);
+      }
+
+
+      [Fact]
+      public void Minus8Ultimate ()
       {
         var garruk = C("Garruk, Apex Predator");
         var bear1 = C("Grizzly Bears");
         var bear2 = C("Grizzly Bears");
 
-        Battlefield(P1, garruk.AddCounters(8, CounterType.Loyalty), bear1, bear2);
+        Battlefield(P1, garruk.AddCounters(9, CounterType.Loyalty), bear1, bear2);
+
         Battlefield(P2, "Grizzly Bears", "Grizzly Bears");
 
         P2.Life = 10;
         
         RunGame(1);
         
-        //Equal(1, C(garruk).Loyalty);
+        Equal(1, C(garruk).Loyalty);
         Equal(7, C(bear1).Power);
         Equal(7, C(bear2).Toughness);
         Equal(0, P2.Life);
---
+++ tests/Grove.Tests/Cards/SigilOfTheEmptyThrone.cs
@@ -0,0 +1,25 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+  using System.Linq;
+
+
+  class SigilOfTheEmptyThrone
+  {
+    public class Ai : AiScenario
+    {
+
+      [Fact]
+      public void Spawn1Angel()
+      {
+        Hand(P1, "Sigil of the Empty Throne");
+        Battlefield(P1, "Sigil of the Empty Throne", "Plains", "Plains","Plains","Plains","Plains");
+
+        RunGame(1);
+
+        Equals(1, P1.Battlefield.Creatures.Count());
+      }
+    }
+  }
+}
