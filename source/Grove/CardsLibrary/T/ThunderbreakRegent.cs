c95d10d81bf860a5f67ba77c0f286a1683c7e11b
Callumhutchy <callumhutchie@hotmail.co.uk>
2021-03-12 00:05:42 +0000

    Added Thunderbreak Regent
    Test doesnt seem to use the cards correctly, I will test in a game to make sure the card triggers correctly

---
+++ source/Grove/CardsLibrary/T/ThunderbreakRegent.cs
@@ -0,0 +1,39 @@
+﻿namespace Grove.CardsLibrary.D
+{
+  using Grove.Effects;
+  using Grove.Triggers;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public class ThunderbreakRegent : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Thunderbreak Regent")
+        .ManaCost("{2}{R}{R}")
+        .Type("Creature - Dragon")
+        .Text("{Flying}{EOL}Whenever a Dragon you control becomes the target of a spell or ability an opponent controls, Thunderbreak Regent deals 3 damage to that player.")
+        .Power(4)
+        .Toughness(4)
+        .TriggeredAbility(p =>
+        {
+          p.Text = "Whenever a Dragon you control becomes the target of a spell or ability an opponent controls, Thunderbreak Regent deals 3 damage to that player.";
+          p.Trigger(new OnBeingTargetedBySpellOrAbility((target, effect, trigger) =>
+          {
+            if (effect.Controller == trigger.Controller)
+              return false;
+
+            return target.IsCard() && target.Card().Is("dragon") && target.Card().Controller == trigger.Controller;
+          }));
+
+          p.TriggerOnlyIfOwningCardIsInPlay = true;
+
+            p.Effect = () => new DealDamageToPlayer(3, P(e=>e.Effect().Controller));
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/T/ThunderbreakRegent.cs
@@ -0,0 +1,34 @@
+﻿namespace Grove.Tests.Cards.T
+{
+  using Grove.Effects;
+  using Grove.Triggers;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+  using Xunit;
+  using Infrastructure;
+
+  public class ThunderbreakRegent 
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void PingOpponent()
+      {
+        var regent = C("Thunderbreak Regent");
+        var plummet = C("Plummet");
+        Hand(P1, plummet);
+        Battlefield(P1, "Forest","Forest");
+        Battlefield(P2, regent);
+        P1.Life = 25;
+        Exec(
+          At(Step.FirstMain)
+            .Cast(plummet, target: regent)
+            .Verify(() =>Equal(22, P1.Life))
+        );
+      }
+    }
+  }
+}
